445916cf4a7f5bae41bd28771d883803437604f5
owenjstl <48632168+owenjstl@users.noreply.github.com>
2022-06-08 16:49:26 -0400

    Add files via upload

---
+++ Code/Stat Data Sci in R/test_embed.R
@@ -0,0 +1,28 @@
+rm(list = ls())
+library(httr)
+library(data.table)
+
+
+
+getEmbeddings <- function(text){
+  input <- list(
+    instances = list( text)
+  )
+  res <- POST("https://dsalpha.vmhost.psu.edu/api/use/v1/models/use:predict", body = input,encode = "json", verbose())
+  emb <- unlist(content(res)$predictions)
+  emb
+}
+
+
+
+
+data <- fread('./project/volume/data/raw/test_data.csv')
+emb_dt<-NULL
+#loops
+for (i in 1:length(data$text)){
+  emb_dt<-rbind(emb_dt,getEmbeddings(data$text[i]))
+}
+
+emb_dt <- data.table(emb_dt)
+
+fwrite(emb_dt,"./project/volume/data/interim/test_emb.csv")
